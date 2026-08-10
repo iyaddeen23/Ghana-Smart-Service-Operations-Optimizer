335dda05f7ae046c751d96dc66db2bc351d87c24
Victor <grantv076@gmail.com>
2026-08-10 00:12:56 +0000

    implemented custom linked list and stack data structures

--- src/main/java/com/campus/optimizer/Main.java
+++ src/main/java/com/campus/optimizer/Main.java
@@ -1,10 +1,31 @@
 package com.campus.optimizer;
 
+import com.campus.optimizer.structures.LinkedList;
+import com.campus.optimizer.structures.Stack;
+
 /**
  * Entry point — launches the console menu.
  */
 public class Main {
     public static void main(String[] args) {
         // TODO: wire up DatabaseConnection, DataLoader, and launch ConsoleMenu
+
+        // Testing Linked List data structure
+        LinkedList<String> list = new LinkedList<>("First");
+        list.add("Second");
+        list.add("Third");
+        
+        System.out.println("Size of the list: " + list.size());
+        System.out.println("Second in the list: " + list.getHead().next.data);
+
+        // Testing stack data structure
+        Stack<String> stack = new Stack<>();
+        stack.push("Victor");
+        stack.push("Wakay");
+        stack.push("Yeng");
+        stack.push("Jace");
+
+        System.out.println("Size of stack: " + stack.count);
+        System.out.println("Top most element is " + stack.top());
     }
 }
--- src/main/java/com/campus/optimizer/structures/PriorityQueue.java
+++ src/main/java/com/campus/optimizer/structures/PriorityQueue.java
@@ -16,80 +16,80 @@ public class PriorityQueue<T extends Comparable<T>> {
     /**
      * Inserts a new element into the priority queue.
      */
-    public void insert(T element) {
-        heap.insert(element);
-        siftUp(heap.size() - 1);
-    }
+    // public void insert(T element) {
+    //     heap.insert(element);
+    //     siftUp(heap.size() - 1);
+    // }
 
     /**
      * Removes and returns the minimum element (highest priority).
      */
-    public T extractMin() {
-        if (isEmpty()) {
-            throw new NoSuchElementException("Priority Queue is empty");
-        }
-        T min = heap.get(0);
-        T last = heap.removeLast();
+    // public T extractMin() {
+    //     if (isEmpty()) {
+    //         throw new NoSuchElementException("Priority Queue is empty");
+    //     }
+    //     T min = heap.get(0);
+    //     T last = heap.removeLast();
         
-        if (heap.size() > 0) {
-            heap.set(0, last);
-            siftDown(0);
-        }
-        return min;
-    }
+    //     if (heap.size() > 0) {
+    //         heap.set(0, last);
+    //         siftDown(0);
+    //     }
+    //     return min;
+    // }
 
     /**
      * Returns the minimum element without removing it.
      */
-    public T peek() {
-        if (isEmpty()) {
-            throw new NoSuchElementException("Priority Queue is empty");
-        }
-        return heap.get(0);
-    }
+    // public T peek() {
+    //     if (isEmpty()) {
+    //         throw new NoSuchElementException("Priority Queue is empty");
+    //     }
+    //     return heap.get(0);
+    // }
 
-    public boolean isEmpty() {
-        return heap.size() == 0;
-    }
+    // public boolean isEmpty() {
+    //     return heap.size() == 0;
+    // }
 
-    public int size() {
-        return heap.size();
-    }
+    // public int size() {
+    //     return heap.size();
+    // }
 
-    private void siftUp(int index) {
-        while (index > 0) {
-            int parentIndex = (index - 1) / 2;
-            if (heap.get(index).compareTo(heap.get(parentIndex)) < 0) {
-                swap(index, parentIndex);
-                index = parentIndex;
-            } else {
-                break;
-            }
-        }
-    }
+    // private void siftUp(int index) {
+    //     while (index > 0) {
+    //         int parentIndex = (index - 1) / 2;
+    //         if (heap.get(index).compareTo(heap.get(parentIndex)) < 0) {
+    //             swap(index, parentIndex);
+    //             index = parentIndex;
+    //         } else {
+    //             break;
+    //         }
+    //     }
+    // }
 
-    private void siftDown(int index) {
-        int minIndex = index;
-        int leftChild = 2 * index + 1;
-        int rightChild = 2 * index + 2;
+    // private void siftDown(int index) {
+    //     int minIndex = index;
+    //     int leftChild = 2 * index + 1;
+    //     int rightChild = 2 * index + 2;
 
-        if (leftChild < heap.size() && heap.get(leftChild).compareTo(heap.get(minIndex)) < 0) {
-            minIndex = leftChild;
-        }
+    //     if (leftChild < heap.size() && heap.get(leftChild).compareTo(heap.get(minIndex)) < 0) {
+    //         minIndex = leftChild;
+    //     }
 
-        if (rightChild < heap.size() && heap.get(rightChild).compareTo(heap.get(minIndex)) < 0) {
-            minIndex = rightChild;
-        }
+    //     if (rightChild < heap.size() && heap.get(rightChild).compareTo(heap.get(minIndex)) < 0) {
+    //         minIndex = rightChild;
+    //     }
 
-        if (index != minIndex) {
-            swap(index, minIndex);
-            siftDown(minIndex);
-        }
-    }
+    //     if (index != minIndex) {
+    //         swap(index, minIndex);
+    //         siftDown(minIndex);
+    //     }
+    // }
 
-    private void swap(int i, int j) {
-        T temp = heap.get(i);
-        heap.set(i, heap.get(j));
-        heap.set(j, temp);
-    }
+    // private void swap(int i, int j) {
+    //     T temp = heap.get(i);
+    //     heap.set(i, heap.get(j));
+    //     heap.set(j, temp);
+    // }
 }
